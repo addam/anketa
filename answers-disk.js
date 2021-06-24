76611982ba79516a1f824239c12804bfe6829027
Adam Dominec <adominec@gmail.com>
2021-06-24 10:05:51 +0200

    time in human-readable format

--- answers-disk.js
+++ answers-disk.js
@@ -4,7 +4,7 @@ function answer(group, user, step, client, content) {
   const time = new Date()
   const name = "otazka"
   const data = Object.keys(content).filter(k => k.startsWith(name)).sort().map(k => content[k].replace(/\r?\n|\r/g, "//"))
-  const result = [Number(time), client, group, user, step, ...data]
+  const result = [time.toJSON(), client, group, user, step, ...data]
   fs.appendFile("data.csv", result.join(",") + "\n", ()=>{})
 }
 
